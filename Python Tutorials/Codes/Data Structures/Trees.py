c31bb2a78e61959d73a4d28a4fb0d67ad6281808
Bunny <varun.verma2020@vitstudent.ac.in>
2024-01-25 08:49:53 +0530

    Renamed notes files to arrange them in order

---
+++ Python Tutorials/Codes/Data Structures/Trees.py
@@ -0,0 +1,10 @@
+class TreeNode:
+    """Class for making TreeNodes."""
+
+    def __init__(self, data):
+        self.parent = None
+        self.child = []
+        self.data = data
+        
+
+    
