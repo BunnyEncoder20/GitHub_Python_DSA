3af18dfd8ee4a26b04720541e29c0dcbee791436
Bunny <varun.verma2020@vitstudent.ac.in>
2024-02-19 13:12:14 +0530

    Completed questions on loops and in depth internal working of python iterables

--- Chai aur Code Tutorials/01_Basics/test.py
+++ Chai aur Code Tutorials/01_Basics/test.py
@@ -1,23 +1,4 @@
-while True : 
-    try : 
-        pet = input("Enter your pet species (cat,dog,fish) :\n ").lower()
-        age = float(input("Enter your pet's age:"))
-
-        if (pet in ['dog','cat','fish']) : 
-            break
-        else : 
-            print("This species is not available, try one of the given species")
-    except ValueError : 
-        print("Enter a valid age for your pet")
-
-if pet=='dog' : 
-    food = 'puppy food' if age<2.0 else 'adult dog food'
-elif pet =='cat' : 
-    food = 'cat food' if age<5.0 else 'senior cat food'
-else :
-    food = 'fish food'
-    
-print(f"Your pet {pet} is recommended to eat {food}")
-
-        
-
+import time 
+print("Soma is my best friend")
+username = "Bunny"
+print(username)
