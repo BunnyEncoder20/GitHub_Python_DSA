0570d49c5ca3d8ef0dad5d291e4df1bcd1ade9e7
Bunny <varun.verma2020@vitstudent.ac.in>
2024-02-19 22:39:16 +0530

    Completed OOP concepts in Python

--- Chai aur Code Tutorials/01_Basics/test.py
+++ Chai aur Code Tutorials/01_Basics/test.py
@@ -1,11 +1,38 @@
-def chaiCoder(num): 
-    def actual(x):
-        return x**num
-    return actual
-
-f = chaiCoder(2)
-g = chaiCoder(3)
-
-print(f(3))
-print(g(3))
+class Car : 
+    def __init__(self, brand, model):
+        self.__brand = brand
+        self.__model = model
+        Car.total += 1
+    __brand = None
+    __model = None
+    total = 0
     
+    def get_brand(self):
+        return self.__brand
+    
+    def get_fullname(self):
+        return f"{self.__brand} {self.__model}"
+
+    @property
+    def model(self):
+        return self.__model
+
+class Battery:
+    def battery_func(self):
+        return "this is from battery class"
+
+class Engine:
+    def engine_func(self):
+        return "this is from engine class"
+
+class ElectricCar(Car, Battery, Engine):
+    def __init__(self, brand, model, battery_size):
+        super().__init__(brand, model)
+        self.battery_size = battery_size
+    battery_size = None
+
+my_tesla = ElectricCar("Tesla", "Model S", "85kWh")
+print(my_tesla.battery_func())
+print(my_tesla.engine_func())
+print(my_tesla.get_fullname())
+
