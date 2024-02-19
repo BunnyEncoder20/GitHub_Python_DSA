548a484aa908f61d7eefde616fde7ab6b49959ec
Bunny <varun.verma2020@vitstudent.ac.in>
2024-02-19 10:35:35 +0530

    Completed 10 problems on conditions

--- Chai aur Code Tutorials/01_Basics/test.py
+++ Chai aur Code Tutorials/01_Basics/test.py
@@ -1,4 +1,23 @@
-keys = ['k1','k2','k3']
-values = ['v1','v2','v3']
-dictionary = {k:v for k,v in zip(keys,values)}
-print(dictionary)
+while True : 
+    try : 
+        pet = input("Enter your pet species (cat,dog,fish) :\n ").lower()
+        age = float(input("Enter your pet's age:"))
+
+        if (pet in ['dog','cat','fish']) : 
+            break
+        else : 
+            print("This species is not available, try one of the given species")
+    except ValueError : 
+        print("Enter a valid age for your pet")
+
+if pet=='dog' : 
+    food = 'puppy food' if age<2.0 else 'adult dog food'
+elif pet =='cat' : 
+    food = 'cat food' if age<5.0 else 'senior cat food'
+else :
+    food = 'fish food'
+    
+print(f"Your pet {pet} is recommended to eat {food}")
+
+        
+
