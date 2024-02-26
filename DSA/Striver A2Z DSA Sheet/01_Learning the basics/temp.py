a3e5da2d53ea27da6df2511638a45816ef294bed
Bunny <varun.verma2020@vitstudent.ac.in>
2024-02-26 07:51:12 +0530

    Made small md for Collectiions.deque

--- DSA/Striver A2Z DSA Sheet/01_Learning the basics/temp.py
+++ DSA/Striver A2Z DSA Sheet/01_Learning the basics/temp.py
@@ -1,12 +1,10 @@
-def symmetry(n: int):
-    
-    for i in range(n*2-1):
-        for j in range(n*2-1):
-            top_distance = i
-            bottom_distance = (n*2-2)-i
-            left_distance = j
-            right_distance = (n*2-2)-j
-            print(n - min(top_distance, bottom_distance, left_distance, right_distance), end=" ")
+def symmetry(n: int) -> None:
+    for i in range(1, n+1):
+        print(" "*(n-i), end="")
+        print("*"*(2*i-1), end="")
+        print(" "*(n-i), end="")
         print()
+    
+    
 if __name__ == '__main__':
     symmetry(4)
