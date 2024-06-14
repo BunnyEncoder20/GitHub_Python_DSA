144dc5a9361cef67617cbf427c8b97283ce88335
Bunny <103634057+BunnyEncoder20@users.noreply.github.com>
2024-06-14 17:18:53 +0530

    Working on  : Reversing a LinkedList in groups of K

--- DSA/Striver SDE DSA Sheet/33 Reverse LinkedList in Groups of Size k/00_brute.py
+++ DSA/Striver SDE DSA Sheet/33 Reverse LinkedList in Groups of Size k/00_brute.py
@@ -1,6 +1,57 @@
 # Actual Problem Code
-def kReverse(head, k):
-    pass
+def kReverse(head,k):
+    temp = head 
+    prev_group_node = None
+
+    while temp != None :
+        kth_node = getKth(temp,k)
+        
+        # The number of nodes remaining are less than k
+        if kth_node == None : 
+            if prev_group_node:
+                prev_group_node.next = temp
+            break
+
+        # Saving the next_group starting node
+        next_group_node = kth_node.next
+
+        # seperating the current group from the remaining LL
+        kth_node.next = None
+
+        reverseLL(temp)
+        printLinkedList(temp)
+
+        # if it is the first group, reassign the new head 
+        # else join the current group to the prev group
+        if temp == head : 
+            head = kth_node
+        else :
+            prev_group_node.next = temp 
+        
+        prev_group_node = temp
+        temp = next_group_node
+    
+    return head
+
+
+def getKth(temp,k):
+    while k != None and k>1:
+        k-=1
+        temp = temp.next
+    return temp
+
+def reverseLL(head):
+    back = Node(None)
+    current = head 
+    front = head.next
+
+    while current != None:
+        front = current.next
+        current.next = back
+        back = current 
+        current = front
+
+    return back     
 
 # Function to print the linked list
 def printLinkedList(head):
@@ -31,7 +82,7 @@ print("Original Linked List: ", end="")
 printLinkedList(head)
 
 # Reverse the linked list
-head = kReverse(head, 4)
+head = kReverse(head,3)
 
 # Print the reversed linked list
 print("Reversed Linked List: ", end="")
--- DSA/Striver SDE DSA Sheet/33 Reverse LinkedList in Groups of Size k/Theory.md
+++ DSA/Striver SDE DSA Sheet/33 Reverse LinkedList in Groups of Size k/Theory.md
@@ -4,3 +4,17 @@
 - `k` is a positive integer and is less than or equal to the length of the linked list. 
 - If the number of nodes is not a multiple of `k` then left-out nodes, in the end, should remain as it is.
 - You may not alter the values in the list's nodes, only nodes themselves may be changed.
+
+<br>
+
+## Brute Force Approach 
+
+### Algorithm 
+
+- [Watch it here](https://youtu.be/lIar1skcQYI?si=f8yB9dDrmb4GePMU&t=132)
+
+### Code 
+
+```python 
+
+```
