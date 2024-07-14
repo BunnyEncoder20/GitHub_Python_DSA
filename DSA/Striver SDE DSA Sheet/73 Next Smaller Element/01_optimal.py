d9202fa8ec2c9eb97196c24d923b68a405e27d89
Bunny WSL2 Machine <varun.verma2020@vitstudent.ac.in>
2024-07-14 21:23:48 +0530

    Finished the Coding problem for NSE

--- DSA/Striver SDE DSA Sheet/73 Next Smaller Element/01_optimal.py
+++ DSA/Striver SDE DSA Sheet/73 Next Smaller Element/01_optimal.py
@@ -1,25 +1,25 @@
-from collections import defaultdict
-
-def get_nse(arr,n):
-    stack = []
-    ans = defaultdict(lambda:-1)
-    
-    for i in range(n-1,-1,-1):
-        if not stack:
-            stack.append(arr[i])
-        elif stack[-1] < arr[i] :
-            ans[arr[i]] = stack[-1]
-            stack.append(arr[i])
-        elif stack[-1] >= arr[i]:
-            while stack and stack[-1] >= arr[i]:
-                stack.pop()
-            if stack:
-                ans[arr[i]] = stack[-1]
-            stack.append(arr[i])
-    res = [ans[num] for num in arr]
-    return res
+def get_nse(arr, n):
+        # Your code goes here
+        # Return the list
+        stack = []
+        nse = [-1]*n
+        
+        for i in range(n-1,-1,-1):
+            if not stack:
+                stack.append(arr[i])
+            elif stack and stack[-1]<arr[i]:
+                nse[i] = stack[-1]
+                stack.append(arr[i])
+            elif stack and stack[-1]>=arr[i]:
+                while stack and stack[-1]>=arr[i]:
+                    stack.pop()
+                if stack : 
+                   nse[i] = stack[-1]
+                stack.append(arr[i])
+        
+        return nse
 
 if __name__ == "__main__":
-    arr = [4,8,5,2,25]
+    arr = [2 ,5 ,3 ,7 ,1 ,5 ,2 ,6 ,3,1]
     res = get_nse(arr,len(arr))
     print(*res)
