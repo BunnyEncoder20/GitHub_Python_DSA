fdb02c3ded72042728aa6be3c6c22dcc1b84564d
Varun Verma <varun.verma2024@gmail.com>
2026-01-13 09:27:38 +0530

    isomorphic strings check done

--- tuf+/basic-strings/isomorphic.py
+++ tuf+/basic-strings/isomorphic.py
@@ -1,6 +1,26 @@
 class Solution:
     def isomorphic(self, s, t):
-        sMap = tMap = {chr(i): 0 for i in range(97, 123)}  # a-z
+        # Trivial case
+        if len(s) != len(t):
+            return False
+
+        sMap = {}
+        tMap = {}
+
+        for i in range(len(s)):
+            sLetter = s[i]
+            tLetter = t[i]
+
+            if sLetter in sMap and sMap[sLetter] != tLetter:
+                return False
+            if tLetter in tMap and tMap[tLetter] != sLetter:
+                return False
+
+            # assign the letters to each other
+            sMap[sLetter] = tLetter
+            tMap[tLetter] = sLetter
+
+        return True
 
 
 if __name__ == "__main__":
