7a18e3f8a2718d1544f6540bb9c87621a25da3cf
Varun Verma <varun.verma2024@gmail.com>
2026-01-17 11:49:18 +0530

    user input basics done

---
+++ ruby-tutorials/user-input.rb
@@ -0,0 +1,6 @@
+puts 'Enter your name:'
+name = gets.chomp # The chomp mehod removes the trailing newline character
+puts 'Hello ' + name + '. You are not like the Common Wealth'
+puts 'Enter your age:'
+age = gets.chomp
+puts 'Damn you are ' + age + ' years old! You are pushing Unc status'
