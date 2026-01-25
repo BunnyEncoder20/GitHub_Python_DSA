1675d7962eda2db6c3d459451f75b7f542d278a8
Varun Verma <varun.verma2024@gmail.com>
2026-01-25 20:14:07 +0530

    control flow chapter done

---
+++ ruby-tutorials/the-odin-project/control_flow.rb
@@ -0,0 +1,19 @@
+# If else block:
+room_tidy = true
+
+if room_tidy
+  puts 'You can play video games!'
+else
+  puts 'Clean your room first!'
+end
+
+# elsif block for connecting values
+attack_by_land = false
+attack_by_sea = false
+if attack_by_land == true
+  puts 'release the goat'
+elsif attack_by_sea == true
+  puts 'release the shark'
+else
+  puts 'release Kevin the flying octopus'
+end
