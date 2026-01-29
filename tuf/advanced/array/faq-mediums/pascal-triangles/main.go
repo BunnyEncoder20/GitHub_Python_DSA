afb4e9d6aafcd9d2759f31f188fc321cf714cffc
Varun Verma <varun.verma2024@gmail.com>
2026-01-29 14:36:59 +0530

    pascals triangle row done

--- tuf/advanced/array/faq-mediums/pascal-triangles/main.go
+++ tuf/advanced/array/faq-mediums/pascal-triangles/main.go
@@ -23,12 +23,33 @@ func nCr(n, r int) int {
 	return result
 }
 
-func pascalTriangles(r, c int) int {
+func pascalTriangles1(r, c int) int {
 	return nCr(r-1, c-1)
 }
 
+func pascalTriangles2(row int) []int {
+	res := make([]int, row)
+	if row > 0 {
+		res[0] = 1 // first and last element of each row is always 1
+	}
+
+	for col := 1; col < row; col++ {
+		res[col] = (res[col-1] * (row - col)) / (col)
+	}
+
+	return res
+}
+
 func main() {
-	fmt.Println(pascalTriangles(4, 2))
-	fmt.Println(pascalTriangles(5, 3))
-	fmt.Println(pascalTriangles(6, 2))
+	fmt.Println("Pascal Triangles 1:")
+	fmt.Println(pascalTriangles1(4, 2))
+	fmt.Println(pascalTriangles1(5, 3))
+	fmt.Println(pascalTriangles1(6, 2))
+
+	fmt.Println("\nPascal Triangles 2:")
+	fmt.Println(pascalTriangles2(4))
+	fmt.Println(pascalTriangles2(5))
+	fmt.Println(pascalTriangles2(6))
+
+	fmt.Println("\nPascal Triangles 3:")
 }
