8cf7c9f11ab21fb02b13f9829f30416fb813b93e
Varun Verma <varun.verma2024@gmail.com>
2026-02-21 17:05:57 +0530

    wrote the better code for median of 2 sorted arrays

--- tuf/advanced/binary-search-II/median-of-two-sorted-arrays/main.go
+++ tuf/advanced/binary-search-II/median-of-two-sorted-arrays/main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func median(arr1 []int, arr2 []int) float64 {
+func bruteMedian(arr1 []int, arr2 []int) float64 {
 	l1, l2 := len(arr1), len(arr2)
 	l3 := l1 + l2
 	arr3 := make([]int, 0, l3)
@@ -37,11 +37,75 @@ func median(arr1 []int, arr2 []int) float64 {
 	}
 }
 
+func betterMedian(arr1 []int, arr2 []int) float64 {
+	len1, len2 := len(arr1), len(arr2)
+
+	// calc the indexs for median
+	idx1, idx2 := ((len1+len2)/2)-1, (len1+len2)/2
+	ele1, ele2 := -1, -1
+	counter := 0
+
+	// make the pointers
+	i, j := 0, 0
+	for i < len1 && j < len2 {
+		if arr1[i] <= arr2[j] {
+			if counter == idx1 {
+				ele1 = arr1[i]
+			}
+			if counter == idx2 {
+				ele2 = arr1[i]
+			}
+			counter++
+			i++
+		} else {
+			if counter == idx1 {
+				ele1 = arr2[j]
+			}
+			if counter == idx2 {
+				ele2 = arr2[j]
+			}
+			counter++
+			j++
+		}
+	}
+
+	// if some elements are left in the other arr
+	for i < len1 {
+		if counter == idx1 {
+			ele1 = arr1[i]
+		}
+		if counter == idx2 {
+			ele2 = arr1[i]
+		}
+		counter++
+		i++
+	}
+	for j < len2 {
+		if counter == idx1 {
+			ele1 = arr2[j]
+		}
+		if counter == idx2 {
+			ele2 = arr2[j]
+		}
+		counter++
+		j++
+	}
+
+	if (len1+len2)%2 == 1 {
+		return float64(ele2)
+	} else {
+		return float64(ele1+ele2) / 2.0
+	}
+}
+
 func main() {
 	arr1, arr2 := []int{2, 4, 6}, []int{1, 3, 5}
-	fmt.Printf("The median of arays %v & %v is %.2f\n", arr1, arr2, median(arr1, arr2))
+	fmt.Printf("The median of arays %v & %v is %.2f\n", arr1, arr2, bruteMedian(arr1, arr2))
+	fmt.Printf("The median of arays %v & %v is %.2f\n", arr1, arr2, betterMedian(arr1, arr2))
 	arr1, arr2 = []int{2, 4, 6}, []int{1, 3}
-	fmt.Printf("The median of arays %v & %v is %.2f\n", arr1, arr2, median(arr1, arr2))
+	fmt.Printf("The median of arays %v & %v is %.2f\n", arr1, arr2, bruteMedian(arr1, arr2))
+	fmt.Printf("The median of arays %v & %v is %.2f\n", arr1, arr2, betterMedian(arr1, arr2))
 	arr1, arr2 = []int{2, 4, 5}, []int{1, 6}
-	fmt.Printf("The median of arays %v & %v is %.2f\n", arr1, arr2, median(arr1, arr2))
+	fmt.Printf("The median of arays %v & %v is %.2f\n", arr1, arr2, bruteMedian(arr1, arr2))
+	fmt.Printf("The median of arays %v & %v is %.2f\n", arr1, arr2, betterMedian(arr1, arr2))
 }
