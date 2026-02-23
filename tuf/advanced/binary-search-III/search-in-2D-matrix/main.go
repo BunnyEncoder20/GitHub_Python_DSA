8a2a60c36304b8ff12cf985f7813dba9c12f1cac
Varun Verma <varun.verma2024@gmail.com>
2026-02-23 11:43:39 +0530

    completed optimal for searching in 2D matrix

--- tuf/advanced/binary-search-III/search-in-2D-matrix/main.go
+++ tuf/advanced/binary-search-III/search-in-2D-matrix/main.go
@@ -33,16 +33,42 @@ func betterSearchMatrix(mat [][]int, target int) bool {
 	return false
 }
 
+func optimalSearchMatrix(mat [][]int, target int) bool {
+	// cause the rows are all sorted and the next row first element will always be more
+	// than the previous rows last element, we can treat the 2D matrix as a 1D sorted array
+	// Hence we can binary search for the the target
+	// idx i will correcspond to the 2D coords: [i/m, i%m]
+	rows, cols := len(mat), len(mat[0])
+	low, high := 0, rows*cols-1
+
+	for low <= high {
+		mid := low + (high-low)/2
+		row, col := mid/cols, mid%cols
+		if mat[row][col] == target {
+			return true
+		} else if mat[row][col] < target {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+
+	return false
+}
+
 func main() {
 	mat, target := [][]int{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}}, 8
 	fmt.Printf("Does target:%d exists within the matrix %v ? %v\n", target, mat, searchMatrix(mat, target))
 	fmt.Printf("Does target:%d exists within the matrix %v ? %v\n", target, mat, betterSearchMatrix(mat, target))
+	fmt.Printf("Does target:%d exists within the matrix %v ? %v\n", target, mat, optimalSearchMatrix(mat, target))
 
 	mat, target = [][]int{{1, 2, 4}, {6, 7, 8}, {9, 10, 34}}, 78
 	fmt.Printf("Does target:%d exists within the matrix %v ? %v\n", target, mat, searchMatrix(mat, target))
 	fmt.Printf("Does target:%d exists within the matrix %v ? %v\n", target, mat, betterSearchMatrix(mat, target))
+	fmt.Printf("Does target:%d exists within the matrix %v ? %v\n", target, mat, optimalSearchMatrix(mat, target))
 
 	mat, target = [][]int{{1, 2, 4}, {6, 7, 8}, {9, 10, 34}}, 7
 	fmt.Printf("Does target:%d exists within the matrix %v ? %v\n", target, mat, searchMatrix(mat, target))
 	fmt.Printf("Does target:%d exists within the matrix %v ? %v\n", target, mat, betterSearchMatrix(mat, target))
+	fmt.Printf("Does target:%d exists within the matrix %v ? %v\n", target, mat, optimalSearchMatrix(mat, target))
 }
